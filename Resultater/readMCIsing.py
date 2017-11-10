3b541a07dea3255091599e9893e763519811c53a
andrgje <aftgjesdal@gmail.com>
2017-11-10 16:26:11 +0100

    filread ferdig

--- Resultater/readMCIsing.py
+++ Resultater/readMCIsing.py
@@ -12,4 +12,11 @@ m_mean=np.transpose(info[:,3:4])
 m_variance=np.transpose(info[:,4:5])
 m_abs=np.transpose(info[:,5:6])
 
+mc_cycles=[]
+for i in range(len(e_mean)):
+	mc_cycles.append(i*1000)
+	
+mc=np.array(mc_cycles)
 
+plt.plot(mc, e_mean)
+plt.show()
