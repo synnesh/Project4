162af0ed6ff945b803c4edb6e8e5a05c6e44777c
synnesh <synnesh@gmail.com>
2017-11-11 15:45:26 +0100

    Legger til Resultater fra 4c

--- Resultater/readMCIsing.py
+++ Resultater/readMCIsing.py
@@ -1,22 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt7
+import matplotlib.pyplot as plt
 
 import sys
 
 info = np.loadtxt(sys.argv[1])
 
 t=np.transpose(info[:,:1])
-e_mean=np.transpose(info[:,1:2])
-e_variance=np.transpose(info[:,2:3]
-m_mean=np.transpose(info[:,3:4])
+e_mean=np.array(info[:,1:2])
+e_variance=np.transpose(info[:,2:3])
+m_mean=np.array(info[:,3:4])
 m_variance=np.transpose(info[:,4:5])
 m_abs=np.transpose(info[:,5:6])
 
-mc_cycles=[]
-for i in range(len(e_mean)):
-	mc_cycles.append(i*1000)
-	
-mc=np.array(mc_cycles)
+mc=np.linspace(0,e_mean.size*1000, e_mean.size)
 
+
+#print mc.shape, e_mean.shape
+plt.figure(1)
 plt.plot(mc, e_mean)
+plt.title("Mean energy as function of Monte Carlo cycles")
+plt.grid()
+plt.xlabel("Number of Monte Carlo cycles")
+plt.ylabel("Mean energy")
+
+plt.figure(2)
+plt.plot(mc, m_mean)
+plt.title("Mean magnetisation as function of Monte Carlo cycles")
+plt.grid()
+plt.xlabel("Number of Monte Carlo cycles")
+plt.ylabel("Mean magnetisation")
 plt.show()
